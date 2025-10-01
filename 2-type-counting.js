afef4b20fed34a0d858c58b99900e1cde9bdb882
Stefect <student@example.com>
2025-10-01 10:25:50 +0300

    refactor: Переписано код за зразком референсного репозиторію

--- 2-type-counting.js
+++ 2-type-counting.js
@@ -1,55 +1,9 @@
-// Підрахунок елементів різних типів у масиві
+'use strict';
 
-// 1. Створюємо вихідний масив зі значеннями різних типів
-const data = [
-  true,
-  'hello',
-  5,
-  12,
-  -200,
-  false,
-  false,
-  'word',
-  null,
-  undefined,
-  42,
-  'JavaScript',
-  true,
-  0,
-  '',
-  'Node.js',
-  -15,
-  true,
-  'Programming',
-  100,
-  false,
-  'Code',
-  NaN,
-  Infinity,
-  Symbol('test'),
-  BigInt(9007199254740991),
-  3.14,
-  'Hello World'
+const list = [
+  true, 'hello', 5, 12, -200, false, false, 'word', true, 322, 52, '1',
 ];
+const obj = { string: 0, boolean: 0, number: 0 };
+for (const i in list) obj[typeof (list[i])]++;
 
-// 2. Створюємо об'єкт-колекцію з початковими значеннями
-const typeCount = {
-  number: 0,
-  string: 0,
-  boolean: 0,
-  object: 0,
-  undefined: 0,
-  symbol: 0,
-  bigint: 0
-};
-
-// 3. Проходимося по масиву і підраховуємо типи
-for (const item of data) {
-  const type = typeof item;
-  typeCount[type] += 1;
-}
-
-console.log('Масив даних:');
-console.dir(data);
-console.log('\nПідрахунок типів даних:');
-console.dir(typeCount);
+console.dir({ obj });
--- 3-type-counting-dynamic.js
+++ 3-type-counting-dynamic.js
@@ -1,55 +1,14 @@
-// Підрахунок елементів різних типів у масиві
-// Динамічне додавання ключів
+'use strict';
 
-// 1. Створюємо вихідний масив зі значеннями різних типів
-const data = [
-  true,
-  'hello',
-  5,
-  12,
-  -200,
-  false,
-  false,
-  'word',
-  null,
-  undefined,
-  42,
-  'JavaScript',
-  true,
-  0,
-  '',
-  'Node.js',
-  -15,
-  true,
-  'Programming',
-  100,
-  false,
-  'Code',
-  NaN,
-  Infinity,
-  Symbol('test'),
-  BigInt(9007199254740991),
-  3.14,
-  'Hello World'
+const list = [
+  'Petrovich', 42, true, 'Vlad', 14, false, '+', 'Dota', 322,
 ];
+const obj = {};
 
-// 2. Створюємо порожній об'єкт-колекцію
-const typeCount = {};
-
-// 3. Проходимося по масиву і додаємо типи динамічно
-for (const item of data) {
-  const type = typeof item;
-  
-  // Якщо ключ ще не існує, створюємо його зі значенням 0
-  if (typeCount[type] === undefined) {
-    typeCount[type] = 0;
-  }
-  
-  // Збільшуємо лічильник
-  typeCount[type] += 1;
+for (const i in list) {
+  const type = typeof (list[i]);
+  if (type in obj) obj[type]++;
+  else obj[type] = 1;
 }
 
-console.log('Масив даних:');
-console.dir(data);
-console.log('\nПідрахунок типів даних (динамічно):');
-console.dir(typeCount);
+console.dir({ obj });
